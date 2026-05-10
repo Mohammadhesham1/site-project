cf7cc82c0eb028f53e3bcfb03452924b05c0a982
Mohammadhesham1 <usermo8650@gmail.com>
2026-05-10 22:43:20 +0300

    Update download.js

--- netlify/functions/download.js
+++ netlify/functions/download.js
@@ -1,17 +1,33 @@
+const https = require('https');
+
 const RAPID_KEY = '6063f46b64msh60311f8db150077p1a9e1djsn408e2c59887f';
+
+function httpsGet(url, headers) {
+  return new Promise((resolve, reject) => {
+    const req = https.get(url, { headers }, (res) => {
+      let data = '';
+      res.on('data', chunk => data += chunk);
+      res.on('end', () => {
+        try { resolve(JSON.parse(data)); }
+        catch(e) { reject(new Error('JSON parse error: ' + data.slice(0,200))); }
+      });
+    });
+    req.on('error', reject);
+    req.setTimeout(10000, () => { req.destroy(); reject(new Error('timeout')); });
+  });
+}
 
 exports.handler = async (event) => {
   const { md5 } = event.queryStringParameters || {};
   if (!md5) return { statusCode: 400, body: JSON.stringify({ error: 'md5 required' }) };
 
+  const url = `https://annas-archive-api.p.rapidapi.com/downloadBook?md5=${md5}`;
+
   try {
-    const res = await fetch(`https://annas-archive-api.p.rapidapi.com/downloadBook?md5=${md5}`, {
-      headers: {
-        'X-RapidAPI-Key': RAPID_KEY,
-        'X-RapidAPI-Host': 'annas-archive-api.p.rapidapi.com',
-      },
+    const data = await httpsGet(url, {
+      'X-RapidAPI-Key': RAPID_KEY,
+      'X-RapidAPI-Host': 'annas-archive-api.p.rapidapi.com',
     });
-    const data = await res.json();
     return {
       statusCode: 200,
       headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' },
